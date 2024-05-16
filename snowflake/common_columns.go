a70836237d6d0b459758b0b2c9c42265196c7a4b
ParthaI <partha@turbot.com>
2024-05-16 23:18:45 +0530

    Added memoize function support and connection key quals

--- snowflake/common_columns.go
+++ snowflake/common_columns.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/memoize"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
@@ -18,14 +19,14 @@ func commonColumns() []*plugin.Column {
 		{
 			Name:        "region",
 			Type:        proto.ColumnType_STRING,
-			Hydrate:     plugin.HydrateFunc(getCommonColumns).WithCache(),
+			Hydrate:     getCommonColumns,
 			Transform:   transform.FromCamel(),
 			Description: "The Snowflake region in which the account is located.",
 		},
 		{
 			Name:        "account",
 			Type:        proto.ColumnType_STRING,
-			Hydrate:     plugin.HydrateFunc(getCommonColumns).WithCache(),
+			Hydrate:     getCommonColumns,
 			Description: "The Snowflake account ID.",
 			Transform:   transform.FromCamel(),
 		},
@@ -37,8 +38,31 @@ type snowflakeCommonColumnData struct {
 	Account, Region string
 }
 
-// get columns which are returned with all tables: region and account
+// if the caching is required other than per connection, build a cache key for the call and use it in Memoize.
+var getCommonColumnsMemoized = plugin.HydrateFunc(getCommonColumnsUncached).Memoize(memoize.WithCacheKeyFunction(getCommonColumnsCacheKey))
+
+// declare a wrapper hydrate function to call the memoized function
+// - this is required when a memoized function is used for a column definition
 func getCommonColumns(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return getCommonColumnsMemoized(ctx, d, h)
+}
+
+// Build a cache key for the call to getCommonColumnsCacheKey.
+func getCommonColumnsCacheKey(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	key := "getCommonColumns"
+	return key, nil
+}
+
+func getAccountForConnection(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	acc, err := getCommonColumnsMemoized(ctx, d, h)
+	if err != nil {
+		return nil, err
+	}
+	return acc.(snowflakeCommonColumnData).Account, nil
+}
+
+// get columns which are returned with all tables: region and account
+func getCommonColumnsUncached(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	config := GetConfig(d.Connection)
 	var region string
 	if config.Region != nil {
